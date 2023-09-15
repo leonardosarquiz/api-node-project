af1b7d54d1189efa790caa78ed68d9f3fe0c669d
Leonardo Sarquiz Hugenthobler <leonardosarquiz14@gmail.com>
2023-09-15 09:43:23 -0300

    added cors

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import './shared/services/TranslationsYup';
 import { router } from './routes';
 import 'dotenv/config';
@@ -6,6 +7,8 @@ import 'dotenv/config';
 
 const server = express();
 
+
+server.use(cors());
 server.use(express.json());
 
 server.use(router);
